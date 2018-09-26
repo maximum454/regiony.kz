e0c3ca2824357e73344fed128359b315e2a17f15
maxim <maximum454@gmail.com>
2018-09-26 12:50:09 +0600

    Главаня и стандартная

---
+++ src/js/regiony.js
@@ -0,0 +1,11 @@
+//= partials/jquery.min.js
+//= partials/popper.min.js
+//= partials/bootstrap.js
+
+$(function () {
+    /*link*/
+    $("a[href='#']").click(function () {
+        return false;
+    });
+
+});
